18a242f627b31c745d613074136a25b28c25b3a6
Aleksandra Sidorova <alexandra.a.sidorova@gmail.com>
2024-01-13 11:33:00 +0100

    Add project files.

---
+++ API_VK_and_API_project.py
@@ -0,0 +1,71 @@
+﻿import configparser
+from tqdm import tqdm
+from urllib.parse import quote, urlencode
+import pyperclip
+import time
+import requests 
+from pprint import pprint
+import os
+import json
+
+# Import spetial created libraries 
+from api import VKAPI
+from api import JSONinfo
+from api import YDAPI
+from local import PCsave
+
+
+if __name__ == '__main__':
+
+    # Reading the complited file woth vk user and tokens info
+    config = configparser.ConfigParser()
+    config.read('config.ini', encoding='utf-8') 
+    vk_id = config["VK"]["vk_id"]
+    token_vk = config["VK"]["token_vk"]
+    token_yandex = config["Yandex Drive"]["token_yandex"]
+    agreement = config["PC"]["agreement"]
+
+    alboms_ids = ['profile', 'wall', 'saved']
+    answer = input(f'Please, choose the locate of photos for saving.\n'
+                   f'- Input "0" if you would like to save {alboms_ids[0]} photos.\n'
+                   f'- Input "1" if you would like to save {alboms_ids[1]} photos.\n'
+                   f'- Input "2" if you would like to save {alboms_ids[2]} photos.\n'
+                   f'Your answer:')
+    
+    atributs = {'VK':   {'vk_id':vk_id,
+                         'version':'5.199',
+                         'token_vk':token_vk},
+                'JSON': {'name_new_folder': 'photos VK'},
+                'YD':   {'token_yandex': token_yandex,
+                         'name_folder_yandex': 'photos VK'}
+                }
+    
+    vk = VKAPI(atributs['VK']['token_vk'],
+               atributs['VK']['vk_id'],
+               atributs['VK']['version'])
+    dict_info = vk.links_photos(album_id = alboms_ids[int(answer)], numbers_getting = 20)
+    
+    # # Just for checking how it works, latter I impruve this: 
+    # print('the list of your VK alboms:', end = '\n')
+    # vk.__alboms_list__()
+
+    js = JSONinfo(dict_info,
+                  atributs['JSON']['name_new_folder'])
+    json_data = js.json_writing()
+
+    yandex = YDAPI(dict_info,
+                   json_data,
+                   atributs['YD']['token_yandex'],
+                   atributs['YD']['name_folder_yandex'])
+    yandex.folder_creating_yandex()
+    yandex.saving_photos_yandex()
+    
+    if agreement == "yes":
+        base_path = config["PC"]["path_pc"]
+        name_new_folder = 'photos VK'
+        pc = PCsave(dict_info,
+                    json_data,
+                    base_path,
+                    name_new_folder)
+        pc.folder_creating()
+        pc.saving_photos()
---
+++ README.md
@@ -0,0 +1,26 @@
+# This script allows to use your VK profile for getting photos and to use your Yandex Drive for downloading last ones 
+
+## Instructions for using:
+### Creating the configuration file
+First of all, use "creating config file.py" for creating file with your own data of tokens and profile info.
+For doing it, follow the instructions shown below:
+1. open and run the "creating config file.py" in a terminal
+2. follow thee instruction on the screen, input all needed data (for helping you, needed browser pages will be opened automatically)
+Great! Now the file with needed data sould be created.
+Note: - Use this "creating config file.py" code again, if you need to update your tokens.
+      - If your saving photos during around 1 hour several times, it usually doesn't requre to change the tokens. 
+
+### Saving photos
+Use "main code.py" for saving photos from VK to your Yandex Drive or your Personal Computer, what you wish. 
+For doing it, follow the instructions shown below:
+1. First of all,  choose the name of VK alboms, which you would like to save (wall, profile, saving)
+5. Than in the script a getting photos information will be implemented automatically, as well as creation json file with info about photos
+6. On your desktop you could see:
+   - status of created a new folder "photos VK" on your Drive
+   - possed bars for saving each photos in that folder on your Drive
+   - status of created a json file "description photos" on your Drive
+7. Finally, the code will save the same data on your computer, if you input "yes" and a path for saving into the configuration file
+
+
+
+
